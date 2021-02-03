95a10873f0a5fdc27bfbdbc89ec01ea29fde07ef
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-02-04 00:07:59 +0100

    🎨 M- added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      maxHeight:{
+        '680px': '680px'
+      },
+      colors:{
+        yellow:{
+          light:'#eee6d4fe'
+        }
+      }, 
+      backgroundImage:{
+        'bike_sharing': "url('/img/bike_sharing_bg_white.png')", 
+      }
+    },
   },
   variants: {
     extend: {},
