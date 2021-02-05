5ca532dcc0e7680234fa88fd7ebc2bdedc57c97a
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-02-05 10:08:03 +0100

    🎨 M- added popup, stationinfo and changed the default coordinates

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,32 +1,33 @@
 import React, {useCallback, useEffect, useState } from 'react';
-import ReactMapGL,{Layer} from 'react-map-gl';
+import ReactMapGL,{Marker, Popup} from 'react-map-gl';
 import api from '@/services/api';
+import Pins from '@/components/pins';
+import StationInfo from '@/components/stationInfo';
 
 const map: React.FC = () => {
-
-  // const parkLayer = {
-  //   id: 'velib',    
-  //   source: '/v2/networks/velib',
-  //   'source-layer': 'networks',
-  //   // filter: ['==', 'class', 'park']
-  // };
-
-  // const [parkColor, setParkColor] =useState('#8fa');
+  const [networks,setNetworks]=useState([]);
+  const[popupInfo, setPopupInfo]=useState(null);
 
   const[viewport, setViewport]=useState(
      {
       width: '100vw',
       height: '100vh',
-      latitude: 37.175296,
-      longitude: -3.489792,
-      zoom: 13
+      // latitude: 37.175296,
+      // longitude: -3.489792,
+      // zoom: 13
+      latitude: 40,
+      longitude: -100,
+      zoom: 3.5,
+      bearing: 0,
+      pitch: 0
     }
   );
 
   const getDataApi=useCallback(async()=>{
     try{
       const response = await api.get('/networks');
-      console.log(response, 'RESPOSTA');
+      console.log(response.data.networks);       
+      setNetworks(response.data.networks)
     }catch(err){
       console.log(err, 'ERROR')
     }
@@ -43,7 +44,21 @@ const map: React.FC = () => {
         onViewportChange={(viewport) => setViewport(viewport )}
         {...viewport}
       >
-         {/* <Layer {...parkLayer} type="fill" paint={{'fill-color': parkColor}} /> */}
+         <Pins data={networks} onClick={setPopupInfo} />
+         {popupInfo && (
+          <Popup
+            className="custom-popup"                       
+            tipSize={5}
+            anchor="top"
+            longitude={popupInfo.longitude}
+            latitude={popupInfo.latitude}
+            closeOnClick={false}
+            onClose={setPopupInfo}
+            closeButton={false}
+          >
+            <StationInfo info={popupInfo} />
+          </Popup>
+        )}      
       </ReactMapGL>
   );
 }
