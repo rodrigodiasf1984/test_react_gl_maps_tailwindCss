0d1a40e99cfe1716350ee02ae0462b9e2a9aeb7c
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-02-09 22:13:01 +0100

    🎨 M- disable rules

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import React from 'react';
 import '../../styles/globals.css';
 import '../../styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
