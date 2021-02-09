04504d71210a9ed9285c4696679a188d2fe6c0c6
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-02-09 22:10:52 +0100

    🎨 M- added types, verification if the network has stations, added component pins

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,171 +1,230 @@
-import React, {useCallback, useEffect, useRef, useState } from 'react';
-import ReactMapGL,{Marker, FlyToInterpolator, Popup} from 'react-map-gl';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import api from '@/services/api';
 import StationInfo from '@/components/stationInfo';
 import useSupercluster from 'use-supercluster';
+import Pins from '@/components/pins';
+import { InfoProps, ViewportTypes } from '@/types';
 
 const Map: React.FC = () => {
   const [points, setPoints] = useState([]);
-  const [selectedMarker, setSelectedMarker]= useState(null);
-  const [viewport, setViewport]= useState(
-     {
-      width: '100vw',
-      height: '100vh',
-      latitude: 40,
-      longitude: -100,
-      zoom:1,
-    }
-  );
+  const [radius, setRadius] = useState(75);
+  const [layerId, setLayerId] = useState<string>('one');
+  const [selectedStation, setSelectedStation] = useState<InfoProps>(null);
+  const [zoom, setZoom] = useState(1);
+  const [viewport, setViewport] = useState<ViewportTypes>({
+    width: '100vw',
+    height: '100vh',
+    latitude: 40,
+    longitude: -100,
+    zoom,
+  });
 
-  const mapRef = useRef();
+  const mapRef = useRef(null);
+  const layers = {
+    one: { radius, maxZoom: 20 },
+    two: { radius, maxZoom: 20 },
+  };
 
-  const getDataApi = async() => {
-    try{
+  const getDataApi = async () => {
+    try {
       const response = await api.get('/networks');
-      const networks = response.data.networks.map(network =>({
-        type: "Feature",
-        properties:{
-          cluster:false,
+      const networks = response.data.networks.map(network => ({
+        type: 'Feature',
+        properties: {
+          cluster: false,
           networkId: network.id,
           country: network.location.country,
           city: network.location.city,
           name: network.name,
         },
-        geometry:{
+        geometry: {
           type: 'Point',
-          coordinates:[
+          coordinates: [
             parseFloat(network.location.longitude),
-            parseFloat(network.location.latitude)
-          ]
-        }
+            parseFloat(network.location.latitude),
+          ],
+        },
       }));
       setPoints(networks);
-      // console.log(response, 'data')
-    }catch(err){
-      console.log(err, 'Error networks')
+    } catch (err) {
+      console.log(err, 'Error networks');
     }
-  }
-  // },[]);
+  };
 
-  const getStations = useCallback(async(network_id) => {
+  const getStations = useCallback(async network_id => {
     try {
       const response = await api.get(`/networks/${network_id}`);
-
-      const respStations = response.data.network.stations.map(station =>({
-        type: "Feature",
-        properties:{
-          cluster:false,
-          name: station.name,
-          free_bikes: station.free_bikes
-        },
-        geometry:{
-          type: 'Station',
-          coordinates:[
-            parseFloat(station.longitude),
-            parseFloat(station.latitude)
-          ]
-        }
-      }));
-      console.log(respStations, 'STATIONS');
-      setPoints(respStations);
+      if (response.data.network.stations.length > 0) {
+        const respStations = response.data.network.stations.map(station => ({
+          type: 'Feature',
+          properties: {
+            cluster: false,
+            name: station.name,
+            free_bikes: station.free_bikes,
+            address: station.extra.address,
+          },
+          geometry: {
+            type: 'Station',
+            coordinates: [
+              parseFloat(station.longitude),
+              parseFloat(station.latitude),
+            ],
+          },
+        }));
+        setPoints(respStations);
+        setLayerId('two');
+        setZoom(13);
+        setRadius(100);
+      } else {
+        alert('No stations found ');
+      }
     } catch (error) {
-      console.log(error, 'Error stations')
+      console.log(error, 'Error stations');
     }
-  },[]);
+  }, []);
 
-  const handleOnCLick=(data )=>{
-    setSelectedMarker(data);
+  const handleOnCLick = data => {
     getStations(data.properties.networkId);
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getDataApi();
-    const listener = e => {
-      if(e.key === "Escape"){
-        setSelectedMarker(null)
+    const listener = (e: { key: string }) => {
+      if (e.key === 'Escape') {
+        setSelectedStation(null);
       }
     };
-    window.addEventListener("keydown", listener);
-    return ()=>{
-      window.removeEventListener("keydown", listener);
-    }
-  }),[];
+    window.addEventListener('keydown', listener);
+    return () => {
+      window.removeEventListener('keydown', listener);
+    };
+  }, []);
 
-  const bounds  = mapRef.current ? mapRef.current?.getMap().getBounds().toArray().flat() : null;
-  const{clusters, supercluster}=useSupercluster({
+  const bounds = mapRef.current
+    ? mapRef.current.getMap().getBounds().toArray().flat()
+    : null;
+
+  const options = `${layerId === 'one' ? layers.one : layers.two}`;
+  const { clusters, supercluster } = useSupercluster({
     points,
-    zoom:viewport.zoom,
+    zoom: viewport.zoom,
     bounds,
-    options:{radius:75, maxZoom:20}
+    options,
   });
-   console.log(clusters);
+
+  const handleBackNetworks = () => {
+    setSelectedStation(null);
+    getDataApi();
+    setLayerId('one');
+    setViewport({
+      ...viewport,
+      zoom: 1,
+      latitude: 40,
+      longitude: -100,
+    });
+  };
   return (
     <ReactMapGL
-        ref={mapRef}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        mapboxApiAccessToken={`${process.env.NEXT_PUBLIC_API_MAP_BOX}`}
-        onViewportChange={(newViewport) => setViewport({...newViewport })}
-        {...viewport}
-        maxZoom={20}
-      >
-        {clusters.map((cluster) => {
-          const[longitude, latitude] = cluster.geometry.coordinates;
-          const{cluster: isCluster, point_count: pointCount} = cluster.properties;
-          const pointSize= `${30+ (pointCount /points.length) * 20}px`;
-          if(isCluster){
-              return(
-                <Marker
-                  key={cluster.id}
-                  latitude={latitude}
-                  longitude={longitude}
-                >
-                  <div className="point" style={{height:pointSize, width:pointSize}}
-                    onClick={()=>{
-                      const expansionZoom  = Math.min(supercluster.getClusterExpansionZoom(cluster.id),20);
-                      setViewport({
-                        ...viewport,
-                        latitude,
-                        longitude,
-                        zoom: expansionZoom,
-                      });
-                    }}
-                  >
-                    {pointCount}
-                  </div>
-                </Marker>
-              )
-            }
-            return(
+      ref={mapRef}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxApiAccessToken={`${process.env.NEXT_PUBLIC_API_MAP_BOX}`}
+      onViewportChange={(newViewport: ViewportTypes) =>
+        setViewport({ ...newViewport })
+      }
+      {...viewport}
+      maxZoom={20}
+    >
+      {clusters.length > 0 &&
+        clusters.map(cluster => {
+          const [longitude, latitude] = cluster.geometry.coordinates;
+          const {
+            cluster: isCluster,
+            point_count: pointCount,
+          } = cluster.properties;
+          const point_size = `${30 + (pointCount / points.length) * 20}px`;
+          if (isCluster) {
+            const bg_point = `${
+              layerId === 'two' ? 'bg-blue-600' : 'bg-green-600'
+            }`;
+            return (
               <Marker
-                key={cluster.properties.networkId}
+                key={cluster.id}
                 latitude={latitude}
                 longitude={longitude}
               >
-                <button onClick={()=>handleOnCLick(cluster)}>
-                  <svg className="w-8 h-8 text-green-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+                <button
+                  type="button"
+                  className={`point ${bg_point}`}
+                  style={{ height: point_size, width: point_size }}
+                  onClick={() => {
+                    const expZoom = layerId === 'two' ? 13 : 2;
+                    const expansionZoom = Math.max(
+                      supercluster.getClusterExpansionZoom(cluster.id),
+                      expZoom,
+                    );
+                    setViewport({
+                      ...viewport,
+                      latitude,
+                      longitude,
+                      zoom: expansionZoom,
+                    });
+                  }}
+                >
+                  {pointCount}
                 </button>
               </Marker>
             );
           }
-        )}
-        {/* {
-          selectedMarker ? (
-            <Popup
-              className="custom-popup"
-              tipSize={5}
-              anchor="top"
-              longitude={selectedMarker.geometry.coordinates[0]}
-              latitude={selectedMarker.geometry.coordinates[1]}
-              closeOnClick={false}
-              onClose={()=>setSelectedMarker(null)}
-              closeButton={true}
+          return (
+            <Marker
+              key={cluster.properties.networkId}
+              latitude={latitude}
+              longitude={longitude}
             >
-              <StationInfo info={selectedMarker} />
-            </Popup>
-          ) : null} */}
-
-      </ReactMapGL>
+              {layerId === 'one' ? (
+                <button type="button" onClick={() => handleOnCLick(cluster)}>
+                  <Pins layerNumber={layerId} />
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  onClick={() => setSelectedStation(cluster)}
+                >
+                  <Pins layerNumber={layerId} />
+                </button>
+              )}
+            </Marker>
+          );
+        })}
+      {selectedStation ? (
+        <Popup
+          className="custom-popup"
+          tipSize={5}
+          anchor="top"
+          longitude={selectedStation.geometry.coordinates[0]}
+          latitude={selectedStation.geometry.coordinates[1]}
+          closeOnClick={false}
+          onClose={() => setSelectedStation(null)}
+          closeButton
+        >
+          <StationInfo info={selectedStation} />
+        </Popup>
+      ) : null}
+      {layerId === 'two' ? (
+        <aside className="flex">
+          <button
+            type="button"
+            className="btn-network"
+            onClick={() => handleBackNetworks()}
+          >
+            <strong className="font-bold">Networks</strong>
+          </button>
+        </aside>
+      ) : null}
+      ;
+    </ReactMapGL>
   );
-}
+};
 
 export default Map;
